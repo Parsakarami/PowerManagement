6a0d65873a8a043b5065f94026754ece8842f3b1
Parsa Karami <Parsakarami@live.com>
2017-07-14 13:58:23 +0430

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.System;
+using System;
+
+namespace PowerManagement
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btn_Restart_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ShutdownManager.BeginShutdown(ShutdownKind.Restart, TimeSpan.FromSeconds(5));
+        }
+
+        private void btn_ShutDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ShutdownManager.BeginShutdown(ShutdownKind.Shutdown, TimeSpan.FromSeconds(5));
+        }
+
+        private void btn_Cancel_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ShutdownManager.CancelShutdown();
+        }
+    }
+}
